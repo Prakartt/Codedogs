0d26e6ba764d094bf00befcffed4a35b03ac8244
SidhantMalik007 <52649988+SidhantMalik007@users.noreply.github.com>
2021-06-27 17:41:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ $(".buy").click(function () {
 $(".remove").click(function () {
   $(".bottom").removeClass("clicked");
 });
+$(".toggle-menu").click(function () {
+  $(this).toggleClass("active");
+  $("#menu").toggleClass("open");
+});
